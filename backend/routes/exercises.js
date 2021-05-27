15eb85c94579ecc9e3be7d0c18cba32d7c606206
yueymiqua <pierre1116@gmail.com>
2021-05-27 09:33:33 -0400

    updated exercise route endpoints

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 
 let Exercise = require('../models/exercise.model');
 
+// get back all exercise object data
 router.route('/').get((req, res) => {
   Exercise.find()
   .then(exercises => res.json(exercises))
   .catch(err => res.status(400).json('Error: ' + err))
 })
 
+// add new exercise to database
 router.route('/add').post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
@@ -26,18 +28,21 @@ router.route('/add').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err))
 });
 
+// get exercise object by id
 router.route('/:id').get((req, res) => {
   Exercise.findById(req.params.id)
   .then(exercise => res.json(exercise))
   .catch(err => res.status(400).json('Error: ' + err))
 });
 
+// deleted exercise object by id
 router.route('/:id').delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
   .then(() => res.json('Exercise Deleted!'))
   .catch(err => res.status(400).json('Error: ' + err))
 })
 
+// update exercise data by id
 router.route('/:id').put((req, res) => {
   Exercise.findById(req.params.id)
   .then(exercise => {
