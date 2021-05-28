b87a10c55cc4fd8361137d469b1c07c2643a56d6
yueymiqua <pierre1116@gmail.com>
2021-05-28 14:52:58 -0400

    updated exercise-list component

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const ExercisesList = () => {
+
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/exercises/')
+      .then(res => {
+        setExercises(res.data);
+        console.log(exercises);
+      }).catch(err => {
+        console.log('Error: ' + err)
+      })
+  },[])
+
+  const deleteExercise = (id) => {
+    axios.delete(`http://localhost:5000/${id}`)
+    .then(res => console.log(res.data))
+
+    setExercises(exercises.filter(exercise => exercise._id !== id))
+  }
+
   return <div>You are on the Exercises List component!</div>
 }
 
