0550f02f562b50cc258fe4facb32b01ddc8c6e68
yueymiqua <pierre1116@gmail.com>
2021-05-29 10:58:14 -0400

    updated edit-exercises component

--- src/components/edit-exercises.component.js
+++ src/components/edit-exercises.component.js
@@ -1,7 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const EditExercises = () => {
-  return <div>You are on the Edit Exercises component!</div>
+const EditExercise = () => {
+
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/exercises/${props.match.params.id}`)
+      .then(res => {
+        setUsername(res.data.username)
+        setDescription(res.data.description)
+        setDuration(res.data.description)
+        setDate(new Date(res.data.date))
+      }).catch(err => console.log('Error: ' + err))
+    axios.get('http://localhost:5000/users')
+      .then(res => {
+        console.log(res);
+        if(res.data.length > 0) {
+          setUsers(
+            res.data.map(user => user.username)
+          )
+        }
+      }).catch(err => console.log('Error: ' + err))
+  },[])
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value);
+  }
+
+  const onChangeDate = (e) => {
+    setDate(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username: username,
+      description: description,
+      duration: duration,
+      date: date
+    }
+
+    console.log(exercise);
+
+    axios.post(`http://localhost:5000/exercises/update/${props.match.params.id}`, exercise)
+      .then(res => console.log(res.data))
+
+    window.location = '/';
+
+  }
+
+  return (
+    <div>
+      <h3>Edit Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select 
+            required 
+            className="form-control" 
+            value={username} 
+            onChange={(e) => onChangeUsername(e)}>
+            {
+              users.map((user) => {
+                return <option key={user} value={user}>{user}</option>
+              })
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={(e) => onChangeDescription(e)}  
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input type="number"
+            required
+            className="form-control"
+            value={duration}
+            onChange={(e) => onChangeDuration(e)}  
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <input type="date"
+            required
+            className="form-control"
+            onChange={(e) => onChangeDate(e)}
+          />
+        </div>
+        <div className="form-group">
+            <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
-export default EditExercises
+export default EditExercise;
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+const Exercise = props => {
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.data.substring(0,10)}</td>
+    <td>
+      <Link to={`/edit/${props.exercise._id}`}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props._id)}}>delete</a>
+    </td>
+  </tr>
+}
+
 const ExercisesList = () => {
 
   const [exercises, setExercises] = useState([]);
@@ -23,7 +35,31 @@ const ExercisesList = () => {
     setExercises(exercises.filter(exercise => exercise._id !== id))
   }
 
-  return <div>You are on the Exercises List component!</div>
+  const exerciseList = () => {
+    return exercises.map(currentExercise => {
+      return <Exercise exercise={currentExercise} deleteExercise={() => deleteExercise()} key={currentExercise._id} />
+    })
+  }
+
+  return (
+    <div>
+      <h3>Logged Exercises</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Duration</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exerciseList}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default ExercisesList;
